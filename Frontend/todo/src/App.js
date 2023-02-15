5ecff1f051e07f8e87a92e77647c743ac11a0d03
Kenneth Rose <rosekenny12@gmail.com>
2023-02-14 23:58:07 -0500

    Create frontend and Task component

---
+++ Frontend/todo/src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+import Task from './components/Task'
+
+function App() {
+	return <div className='App'></div>
+}
+
+export default App
---
+++ Frontend/todo/src/components/Task.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import '../styles/Task.css'
+
+function Task(props) {
+	const [completed, setCompleted] = useState(props.completed)
+
+	function handleCheckboxChange() {
+		setCompleted(!completed)
+	}
+
+	const [dueDate, setDueDate] = useState(props.dueDate)
+
+	const handleDateChange = (date) => {
+		setDueDate(date)
+		const dateString = date.toLocaleDateString('en-US', {
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit',
+		})
+
+		console.log(dateString)
+	}
+
+	const [description, setDescription] = useState(props.description)
+
+	function handleDescriptionChange(event) {
+		setDescription(event.target.value)
+	}
+
+	return (
+		<div className='task-card'>
+			<div className='task-content'>
+				<input
+					className='task-completed'
+					type='checkbox'
+					checked={completed}
+					onChange={handleCheckboxChange}
+				/>
+				<textarea
+					className='task-description'
+					value={description}
+					onChange={handleDescriptionChange}
+				/>
+				<DatePicker
+					selected={dueDate}
+					onChange={handleDateChange}
+					className='task-due-date'
+				/>
+			</div>
+		</div>
+	)
+}
+
+export default Task
